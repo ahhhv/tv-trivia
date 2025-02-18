d08bc68d9c6dbdd6484d122bd62c85d953f3afe7
Alex Hernández <ahernandezv85@gmail.com>
2025-02-18 10:54:58 +0100

    Initial Commit

---
+++ tvcrowd/tvcrowdApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvcrowdApp.swift
+//  tvcrowd
+//
+//  Created by Alex Hernández on 18/2/25.
+//
+
+import SwiftUI
+
+@main
+struct tvcrowdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tvcrowdTests/tvcrowdTests.swift
@@ -0,0 +1,17 @@
+//
+//  tvcrowdTests.swift
+//  tvcrowdTests
+//
+//  Created by Alex Hernández on 18/2/25.
+//
+
+import Testing
+@testable import tvcrowd
+
+struct tvcrowdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
